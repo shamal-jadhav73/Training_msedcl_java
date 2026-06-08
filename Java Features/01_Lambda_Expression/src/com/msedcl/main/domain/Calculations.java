b00edcfb6be2eead0b649eef42ab88c667e35dcf
shamal-jadhav73 <shamal.jadhav73@gmail.com>
2026-06-08 17:05:02 +0530

    Delete Java Features directory

--- Java Features/01_Lambda_Expression/src/com/msedcl/main/domain/Calculations.java
+++
@@ -1,6 +0,0 @@
-package com.msedcl.main.domain;
-
-public interface Calculations {
-	double doCalculations(double numberOne,double numberTwo);
-
-}
--- Java Features/01_Lambda_Expression/src/com/msedcl/main/domain/GstCalculation.java
+++
@@ -1,7 +0,0 @@
-package com.msedcl.main.domain;
-
-public interface GstCalculation {
-	
-	public double calculate(int units);
-
-}
