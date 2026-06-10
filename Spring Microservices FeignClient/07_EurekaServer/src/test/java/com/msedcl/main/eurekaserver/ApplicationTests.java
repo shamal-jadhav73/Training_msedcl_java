8083b22b8246cf08795c07a19c5f10f312c5ac20
shamal-jadhav73 <shamal.jadhav73@gmail.com>
2026-06-10 17:50:42 +0530

    Add files via upload

---
+++ Spring Microservices FeignClient/07_EurekaServer/src/test/java/com/msedcl/main/eurekaserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msedcl.main.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
